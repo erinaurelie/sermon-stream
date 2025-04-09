3c8a9b828682882638a99bd6c59921a4b16dd7f7
Erin Aurélie <erinaurelieessama@gmail.com>
2025-04-09 21:43:11 +0000

    setup config file

---
+++ config.js
@@ -0,0 +1,6 @@
+// API key is restricted to this github page 
+const API_KEY = 'AIzaSyDf_HCLnGlXR2OUZQKuaj47csbMqxdS8CM'; 
+const UPLOADS_PLAYLIST_ID = 'UU0nE_KlFn35is_mpkaQ6YaQ';
+
+
+export { API_KEY, UPLOADS_PLAYLIST_ID }
